e73ed6794b82a11b13e52d3358e606a65d76f1c3
Mitch Talmadge <mitch@talmad.ge>
2021-03-05 01:01:54 -0700

    Make selenium look more like a normal browser.

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,16 @@ class StockBot {
   private async init() {
     this.driver = await new Selenium.Builder()
       .forBrowser("chrome")
-      .setChromeOptions(new Options().headless().addArguments("--no-sandbox"))
+      .setChromeOptions(new Options()
+        .headless()
+        .addArguments("--disable-blink-features=AutomationControlled")
+        .addArguments("--disable-dev-shm-usage")
+        .addArguments("--no-sandbox")
+        .addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36")
+        .addArguments("--user-data-dir=/tmp/selenium-user-data")
+        .addArguments("--window-position=0,0")
+        .addArguments("--window-size=1920,1080")
+      )
       .build()
   }
 
